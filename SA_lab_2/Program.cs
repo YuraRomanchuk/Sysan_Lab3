7c0a3d026bdf2d756e4e7b3e8f123d9b4c637ae8
YuraRomanchuk <33698979+YuraRomanchuk@users.noreply.github.com>
2018-12-03 12:24:41 +0200

    add comment

--- SA_lab_2/Program.cs
+++ SA_lab_2/Program.cs
@@ -17,6 +17,7 @@ namespace SA_lab_2
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            // new commit
         }
     }
 }
